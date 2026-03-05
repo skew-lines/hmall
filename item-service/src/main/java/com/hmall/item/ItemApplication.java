b132a699f9b5577c4d21aaa6505b4950c32e9460
skewlines <144216697+skew-lines@users.noreply.github.com>
2026-03-05 15:55:47 +0800

    init(拆分商品服务和购物车服务)

---
+++ item-service/src/main/java/com/hmall/item/ItemApplication.java
@@ -0,0 +1,15 @@
+package com.hmall.item;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//替代@Mapper
+@MapperScan("com.hmall.item.mapper")
+@SpringBootApplication
+public class ItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemApplication.class, args);
+    }
+
+}
